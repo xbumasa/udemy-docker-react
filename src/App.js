e56a01359afa84b2719d0c5fd7bac7fb5256c6f1
Semion Shulman <shulman.dev@gmail.com>
2021-01-25 17:41:32 +0200

    FEATURE #5 branch

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          FEATURE #3! Edit <code>src/App.js</code> and save to reload.
+          FEATURE #5! Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
